6dd94cbf6545db07433e7e94826cf1f5ac632c76
Liam Galvin <liam.galvin@aquasec.com>
2021-10-01 16:30:38 +0100

    Add IAM

---
+++ internal/app/cfsec/adapter/aws/iam/iam.go
@@ -0,0 +1,73 @@
+package iam
+
+import (
+	"fmt"
+
+	"github.com/aquasecurity/cfsec/internal/app/cfsec/parser"
+	"github.com/aquasecurity/cfsec/internal/app/cfsec/util"
+	"github.com/aquasecurity/defsec/provider/aws/iam"
+)
+
+func Adapt(cfFile parser.FileContext) iam.IAM {
+	return iam.IAM{
+		Policies:      getPolicies(cfFile),
+		RolePolicies:  getRolePolicies(cfFile),
+		UserPolicies:  getUserPolicies(cfFile),
+		GroupPolicies: getGroupPolicies(cfFile),
+	}
+}
+
+func retrieveDocument(prop *parser.Property, resource *parser.Resource) (*iam.PolicyDocument, error) {
+	policyDoc := util.GetJsonBytes(prop, resource.SourceFormat())
+	fmt.Printf("%s\n\n", string(policyDoc))
+	return iam.ParsePolicyDocument(policyDoc, prop.Metadata())
+}
+
+func getPolicies(ctx parser.FileContext) (policies []iam.Policy) {
+	for _, policyResource := range ctx.GetResourceByType("AWS::IAM::Policy") {
+		var policy iam.Policy
+		policyProp := policyResource.GetProperty("PolicyDocument")
+		if doc, err := retrieveDocument(policyProp, policyResource); err == nil {
+			policy.Document = *doc
+		}
+		policies = append(policies, policy)
+	}
+	return policies
+}
+
+func getRolePolicies(ctx parser.FileContext) (policies []iam.RolePolicy) {
+	for _, policyResource := range ctx.GetResourceByType("AWS::IAM::RolePolicy") {
+		var policy iam.RolePolicy
+		policyProp := policyResource.GetProperty("PolicyDocument")
+		if doc, err := retrieveDocument(policyProp, policyResource); err == nil {
+			policy.Document = *doc
+		}
+		policies = append(policies, policy)
+	}
+	return policies
+}
+
+func getUserPolicies(ctx parser.FileContext) (policies []iam.UserPolicy) {
+	for _, policyResource := range ctx.GetResourceByType("AWS::IAM::UserPolicy") {
+		var policy iam.UserPolicy
+		policyProp := policyResource.GetProperty("PolicyDocument")
+		if doc, err := retrieveDocument(policyProp, policyResource); err == nil {
+			policy.Document = *doc
+		}
+		policies = append(policies, policy)
+	}
+	return policies
+
+}
+
+func getGroupPolicies(ctx parser.FileContext) (policies []iam.GroupPolicy) {
+	for _, policyResource := range ctx.GetResourceByType("AWS::IAM::GroupPolicy") {
+		var policy iam.GroupPolicy
+		policyProp := policyResource.GetProperty("PolicyDocument")
+		if doc, err := retrieveDocument(policyProp, policyResource); err == nil {
+			policy.Document = *doc
+		}
+		policies = append(policies, policy)
+	}
+	return policies
+}

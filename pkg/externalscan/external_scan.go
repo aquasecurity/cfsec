2c7199751102c5f61756a522bb64d88023a8ade0
Owen Rumney <owen.rumney@aquasec.com>
2021-11-10 13:04:26 +0000

    Add capture of panics (#19)

--- pkg/externalscan/external_scan.go
+++ pkg/externalscan/external_scan.go
@@ -1,6 +1,9 @@
 package externalscan
 
 import (
+	"fmt"
+
+	"github.com/aquasecurity/cfsec/internal/app/cfsec/debug"
 	_ "github.com/aquasecurity/cfsec/internal/app/cfsec/loader"
 	"github.com/aquasecurity/cfsec/internal/app/cfsec/parser"
 	"github.com/aquasecurity/cfsec/internal/app/cfsec/scanner"
@@ -20,6 +23,12 @@ func NewExternalScanner(options ...Option) *ExternalScanner {
 }
 
 func (t *ExternalScanner) Scan(toScan string) ([]result.Result, error) {
+	defer func() {
+		if r := recover(); r != nil {
+			debug.Log("error: %v", r)
+			fmt.Printf("an error was encountered scanning %s\n", toScan)
+		}
+	}()
 	fileContexts, err := parser.NewParser().ParseFiles(toScan)
 	if err != nil {
 		return nil, err
